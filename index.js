0fc6da55a8405c5840b3e3e2436b9b0c3493b589
robertmtpaul <rob@glo.id.au>
2020-09-18 20:14:08 +1000

    Installed Nodemon, Axios.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Express server initialisation
 const express = require('express');
+const { Server } = require('http');
 const app = express();
-// const { Server } = require('http');
+const axios = require('axios');
 
 const PORT = 1337;
+
+app.get('/', (request,response) => {
+    console.log('get/')
+    response.send('welcome to the home page')
+});
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}...`)
