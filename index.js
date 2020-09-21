4659ca632da55e3973cc4b56ea38499b536f5f0f
robertmtpaul <rob@glo.id.au>
2020-09-21 11:32:07 +1000

    Changes to seed pictures

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-// Express server initialisation
 const express = require('express');
-const { Server } = require('http');
 const app = express();
-const axios = require('axios');
-
 const PORT = 1337;
+const data = require('./data.js');
 
-app.get('/', (request,response) => {
-    console.log('get/')
-    response.send('welcome to the home page')
+//Prevent CORS errors from frontend requests.
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json()); //enable support for JSON-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Express server initialisation
+// app.get('/', (request,response) => {
+//     console.log('get/')
+//     response.send('welcome to the home page')
+// });
+
+app.get("/products", (req, res) => {
+    res.json(data.products);
 });
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}...`)
+    console.log('server started at http://localhost:1337')
 })
+
